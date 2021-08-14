4d5612b51357f6c9c2240063c51e7c0f18a48738
buruyuki <buruaceyuki@gmail.com>
2021-08-14 17:05:26 +0900

    html modify to php

--- php/common/header.php
+++ php/common/header.php
@@ -8,13 +8,13 @@
     <div id="gnav-content">
         <ul class="gnav-list">
 
-            <li><a href="../front.html">TOP</a></li>
+            <li><a href="front.php">TOP</a></li>
             <li><a href="../index.html">ブラザーズとは</a></li>
             <li><a href="news.php">お知らせ</a></li>
             <li><a href="member.php">メンバー</a></li>
-            <li><a href="../history.html">歴史</a></li>
+            <li><a href="history.php">歴史</a></li>
             <li><a href="registerForm.php">団員募集</a></li>
-            <li><a href="../faq.html">よくある質問</a></li>
+            <li><a href="faq.php">よくある質問</a></li>
         </ul>
     </div>
 </nav>
