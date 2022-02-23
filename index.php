0ef2aec826458f3b0190cc6c7caf5d1438201ae6
buruyuki <buruaceyuki@gmail.com>
2022-02-23 21:31:19 +0900

    index.phpの位置を変更、余計なファイルを削除

--- index.php
+++ index.php
@@ -1,1 +1,66 @@
-test php
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>broters</title>
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/spm.css" type="text/css" media="screen and (max-width: 375px)">
+    <link rel="icon" href="img/fabicon.ico">
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <!-- slick -->
+    <link rel="stylesheet" type="text/css" href="css/slick/slick.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/slick/slick-theme.css" media="screen" />
+    <script src="css/slick/slick.min.js"></script>
+    <script src="js/common.js"></script>
+
+</head>
+
+<body>
+    <div class="wrapper">
+        <?php include('php/common/header.php'); ?>
+        <section>
+            <h2 class="heading-title">BROTHERSとは？</h2>
+            <!-- https://digipress.info/tech/how-to-include-youtube-vimeo-video-with-autoplay-slick-js/ -->
+            <!-- https://www.will3in.co.jp/frontend-blog/article/shrink-webpage-with-iframe-responsive/ -->
+            <div class="slider">
+                <div class="pc">
+                    <div class="frame">
+                        <iframe class="embed-player slide-media" src="https://www.youtube.com/embed/aqQcaak6GSw" width="1280" height="800" frameborder="0"></iframe>
+                    </div>
+                </div>
+                <div class="pc">
+                    <div class="frame">
+                        <iframe class="embed-player slide-media" src="https://www.youtube.com/embed/X06h5gA0RZs" width="1280" height="800" frameborder="0"></iframe>
+                    </div>
+                </div>
+            </div>
+            <div class="text_area">
+                <p>
+                    BROTHERSとは超ゆるふわ特殊生命体であり、起源は1994年。<br>
+                    人間がごく普通のヌイグルミと共に生活をしていたのだが、ある日突然、魂が宿り始めた。<br>
+                    しかし、魂は元から遠い時空の彼方に宿り続けていたのだが、人間界からの時空干渉が原因により人間界とヌイグルミ国が繋がったのであった。<br>
+                    ヌイグルミ国では長年にわたり、悪の組織から王国を守べく日々戦争が絶え間なく行われていたのだが、人間界からの干渉を受けた事で様々な技術革新が行われた。<br>
+                    その結果、ヌイグルミ王国は悠久の平和を手に入れることに成功したのだ。<br>人間界にはその恩恵として癒しと平和のエネルギーがヌイグルミ王国より供給され、今日に至るまで友好な相互関係が築かれつつある。
+                </p>
+            </div>
+            <?php include('php/common/footer.php'); ?>
+        </section>
+    </div>
+</body>
+
+</html>
+
+<script>
+    jQuery(function($) {
+        $(window).on('load resize', function() {
+            var pc_w = $('.pc .frame').width() / 1280;
+            $('.pc iframe').css('transform', 'scale(' + pc_w + ')');
+        });
+    });
+</script>
--- php/common/header.php
+++ php/common/header.php
@@ -1,6 +1,6 @@
 <div class="main">
     <header>
-        <h1><a href="/php/index.php">BROTHERS</a></h1>
+        <h1><a href="/index.php">BROTHERS</a></h1>
     </header>
     <nav>
         <input id="gnav-input" type="checkbox" class="gnav-hidden">
@@ -9,12 +9,12 @@
         <div id="gnav-content">
             <ul class="gnav-list">
                 <!-- <li><a href="front.php">TOP</a></li> -->
-                <li><a href="index.php">ブラザーズとは</a></li>
-                <li><a href="news.php">お知らせ</a></li>
-                <li><a href="member.php">メンバー</a></li>
-                <li><a href="history.php">歴史</a></li>
-                <li><a href="registerForm.php">団員募集</a></li>
-                <li><a href="faq.php">よくある質問</a></li>
+                <li><a href="/index.php">ブラザーズとは</a></li>
+                <li><a href="/php/news.php">お知らせ</a></li>
+                <li><a href="/php/member.php">メンバー</a></li>
+                <li><a href="/php/history.php">歴史</a></li>
+                <li><a href="/php/registerForm.php">団員募集</a></li>
+                <li><a href="/php/faq.php">よくある質問</a></li>
             </ul>
         </div>
     </nav>
