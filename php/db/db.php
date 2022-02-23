90c9cffd393a33af40355c4c7d5985dca2a749a9
buruyuki <buruaceyuki@gmail.com>
2022-02-23 21:49:59 +0900

    db.phpを環境変数から取得

---
+++ php/db/db.php
@@ -0,0 +1,22 @@
+<?php
+$host = getenv('HOST');
+$dbName = getenv('DBNAME');
+$user = getenv('USER');
+$password = getenv('PASSWORD');
+$dsn = "pgsql:host={$host};dbname={$dbName}";
+
+// tryにPDOの処理を記述
+try {
+
+  // PDOインスタンスを生成
+  $dbh = new PDO($dsn, $user, $password);
+
+  // エラー（例外）が発生した時の処理を記述
+} catch (PDOException $e) {
+
+  // エラーメッセージを表示させる
+  echo 'データベースにアクセスできません！' . $e->getMessage();
+
+  // 強制終了
+  exit;
+}
