c4ec637196d2f96d4d1bc77bf0d4eda6c52fd929
buruyuki <buruaceyuki@gmail.com>
2024-07-13 20:09:14 +0900

    文字化けを解消

--- php/signUpForm.php
+++ php/signUpForm.php
@@ -1,29 +1,41 @@
-<h1>新規会員登録</h1>
-<form action="registerUser.php" method="post">
-    <div>
-        <label>
-            名前：
-            <input type="text" name="name" required>
-        </label>
-    </div>
-    <div>
-        <label>
-            メールアドレス：
-            <input type="text" name="mail" required>
-        </label>
-    </div>
-    <div>
-        <label>
-            パスワード：
-            <input type="password" name="pass" required>
-        </label>
-    </div>
-    <div>
-        <label>
-            ブラザーズID：
-            <input type="" name="brotherspass" required>
-        </label>
-    </div>
-    <input type="submit" value="新規登録">
-</form>
-<p>すでに会員登録済みの方は<a href="loginForm.php">こちら</a></p>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>新規会員登録</title>
+</head>
+
+<body>
+
+    <form action="registerUser.php" method="post">
+        <div>
+            <label>
+                名前：
+                <input type="text" name="name" required>
+            </label>
+        </div>
+        <div>
+            <label>
+                メールアドレス：
+                <input type="text" name="mail" required>
+            </label>
+        </div>
+        <div>
+            <label>
+                パスワード：
+                <input type="password" name="pass" required>
+            </label>
+        </div>
+        <div>
+            <label>
+                ブラザーズID：
+                <input type="" name="brotherspass" required>
+            </label>
+        </div>
+        <input type="submit" value="新規登録">
+    </form>
+    <p>すでに会員登録済みの方は<a href="loginForm.php">こちら</a></p>
+</body>
+
+</html>
